59d1b8a2bd0145e4081d7ea0b8ef0a705f207fb3
An Bui <49201952+AnXBui@users.noreply.github.com>
2021-03-12 01:06:35 -0500

    Image Query Successful

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
-import Jobs from "../components/Jobs"
+import Artworks from "../components/Artworks"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 export default () => {
   return <Layout>
     <Hero/>
     <Services/>
+    <Artworks/>
   </Layout>
 }
 // ...GatsbyImageSharpFluid
